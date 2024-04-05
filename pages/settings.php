258d01e502c40fba40cd80a95a5edef2169aadff
Something226 <axel.lieback@gmail.com>
2024-04-05 13:48:28 +0200

    Changes in settings.php

--- pages/settings.php
+++ pages/settings.php
@@ -54,10 +54,10 @@
           <div class="col-auto my-auto">
             <div class="h-100">
               <h5 class="mb-1">
-                Alec Thompson
+                Admin
               </h5>
               <p class="mb-0 font-weight-bold text-sm">
-                CEO / Co-Founder
+                Admin
               </p>
             </div>
           </div>
