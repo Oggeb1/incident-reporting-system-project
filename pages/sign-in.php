5b4a5a8b0239a846376de5d2c2bdc598cdd07a90
oggeb1 <nej@nej.se>
2024-05-10 12:56:39 +0200

    Fixed a log-in bug and indentation

--- pages/sign-in.php
+++ pages/sign-in.php
@@ -57,40 +57,50 @@ if (isset($_POST['login'])) {
 
     // Get the password for the $username from DB
     $dbUser = $db->execute_query("SELECT password, userType, userID FROM user WHERE userName = ?", [$username])->fetch_assoc();
-    $dbUserPassword = $dbUser['password'];
-    $dbUserType = $dbUser['userType'];
-    $dbUserID = $dbUser['userID'];
 
-    // Check if query exists, if not show error
-    if ($dbUserPassword != null && $dbUserType != null) {
-        // Check if DB passwd and user input password matches
-        if (password_verify($password, $dbUserPassword)) {
-            // Prevent session fixation attack
-            session_regenerate_id();
+    if ($dbUser != null) { // If the query worked
+        // Define variables from query
+        $dbUserPassword = $dbUser['password'];
+        $dbUserType = $dbUser['userType'];
+        $dbUserID = $dbUser['userID'];
 
-            // Set session variables
-            $_SESSION['username'] = $username;
-            $_SESSION['Logged-in'] = true;
-            $_SESSION['userType'] = $dbUserType;
+        // Check if query exists, if not show error
+        if ($dbUserPassword != null && $dbUserType != null) {
+            // Check if DB passwd and user input password matches
+            if (password_verify($password, $dbUserPassword)) {
+                // Prevent session fixation attack
+                session_regenerate_id();
 
-            if ($_POST['rememberMe'] === 'on') {
-                $selector = substr(bin2hex(openssl_random_pseudo_bytes(50)), 0, 12);
+                // Set session variables
+                $_SESSION['username'] = $username;
+                $_SESSION['Logged-in'] = true;
+                $_SESSION['userType'] = $dbUserType;
 
-                $validator = bin2hex(openssl_random_pseudo_bytes(30));
-                $hashedValidator = password_hash($validator, PASSWORD_DEFAULT);
+                // Remind me
+                if ($_POST['rememberMe'] === 'on') { // Check if the remind-me checkbox is ticked
+                    // Generate a "random" selector and validator
+                    $selector = substr(bin2hex(openssl_random_pseudo_bytes(50)), 0, 12);
+                    $validator = bin2hex(openssl_random_pseudo_bytes(30));
+                    // A hashed version of validator to protect against DB-leak
+                    $hashedValidator = password_hash($validator, PASSWORD_DEFAULT);
 
-                $expiryEpoch = time() + 60 * 60 * 24 * 30;
-                $expiry = date('Y-m-d H:i:s', $expiryEpoch);
+                    // Set expiry time in seconds, then convert to "real"-date
+                    $expiryEpoch = time() + 60 * 60 * 24 * 30;
+                    $expiry = date('Y-m-d H:i:s', $expiryEpoch);
 
-                $db->execute_query("INSERT INTO userTokens(userID, selector, validator, expiry) VALUES ((?), (?), (?), (?))", [$dbUserID, $selector, $hashedValidator, $expiry]);
-                $token = $selector . $validator;
-                setcookie("token", $token, $expiryEpoch, secure: true);
+                    // Add the selector and hashed validator to DB
+                    $db->execute_query("INSERT INTO userTokens(userID, selector, validator, expiry) VALUES ((?), (?), (?), (?))", [$dbUserID, $selector, $hashedValidator, $expiry]);
+                    $token = $selector . $validator; // Concatenate selector and validator to be able to store it as one cookie
+                    setcookie("token", $token, $expiryEpoch, secure: true); // Set the cookie
+                }
+                // Log-in successful, redirect to dashboard
+                header("Location: dashboard.php");
+                exit;
+            } else {
+                // Show alert
+                echo "<script type='text/javascript'>alert('Login failed');</script>";
             }
-
-            header("Location: dashboard.php");
-            exit;
         } else {
-            // Show alert
             echo "<script type='text/javascript'>alert('Login failed');</script>";
         }
     } else {
@@ -102,104 +112,104 @@ if (isset($_POST['login'])) {
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-  <title>
-    Soft UI Dashboard by Creative Tim
-  </title>
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-  <!-- Nucleo Icons -->
-  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- Font Awesome Icons -->
-  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- CSS Files -->
-  <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.7" rel="stylesheet" />
-  <!-- Nepcha Analytics (nepcha.com) -->
-  <!-- Nepcha is an easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
-  <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <title>
+        Soft UI Dashboard by Creative Tim
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.7" rel="stylesheet" />
+    <!-- Nepcha Analytics (nepcha.com) -->
+    <!-- Nepcha is an easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+    <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
 
 <body class="">
-  <main class="main-content  mt-0">
+<main class="main-content  mt-0">
     <section>
-      <div class="page-header min-vh-75">
-        <div class="container">
-          <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
-            <div class="card card-plain mt-4">
-              <div class="card-header pb-0 text-center bg-transparent">
-                <h3 class="font-weight-bolder text-primary text-gradient">Incident Portal</h3>
-                <p class="mb-0">Enter your email and password to sign in</p>
-              </div>
-              <div class="card-body">
-                <form method="POST" role="form">
-                  <label>Username</label>
-                  <div class="mb-3">
-                    <input name="username" type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="username-addon" required>
-                  </div>
-                  <label>Password</label>
-                  <div class="mb-3">
-                    <input name="password" type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon" required>
-                  </div>
-                  <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="rememberMe" name="rememberMe" checked="">
-                    <label class="form-check-label" for="rememberMe">Remember me</label>
-                  </div>
-                  <div class="text-center">
-                    <button type="submit" name="login" class="btn bg-gradient-primary w-100 mt-4 mb-0">Sign in</button>
-                  </div>
-                </form>
-              </div>
+        <div class="page-header min-vh-75">
+            <div class="container">
+                <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+                    <div class="card card-plain mt-4">
+                        <div class="card-header pb-0 text-center bg-transparent">
+                            <h3 class="font-weight-bolder text-primary text-gradient">Incident Portal</h3>
+                            <p class="mb-0">Enter your email and password to sign in</p>
+                        </div>
+                        <div class="card-body">
+                            <form method="POST" role="form">
+                                <label>Username</label>
+                                <div class="mb-3">
+                                    <input name="username" type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="username-addon" required>
+                                </div>
+                                <label>Password</label>
+                                <div class="mb-3">
+                                    <input name="password" type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon" required>
+                                </div>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="rememberMe" name="rememberMe" checked="">
+                                    <label class="form-check-label" for="rememberMe">Remember me</label>
+                                </div>
+                                <div class="text-center">
+                                    <button type="submit" name="login" class="btn bg-gradient-primary w-100 mt-4 mb-0">Sign in</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </section>
-  </main>
-  <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
-  <footer class="footer py-5">
+</main>
+<!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+<footer class="footer py-5">
     <div class="container">
-      <div class="row">
-        <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
-          <a href="https://github.com/Oggeb1/incident-reporting-system-project" target="_blank" class="text-secondary">
-            <span class="text-lg fab fa-github"></span>
-          </a>
+        <div class="row">
+            <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+                <a href="https://github.com/Oggeb1/incident-reporting-system-project" target="_blank" class="text-secondary">
+                    <span class="text-lg fab fa-github"></span>
+                </a>
+            </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-8 mx-auto text-center mt-1">
-          <p class="mb-0 text-secondary">
-            Copyright © <script>
-              document.write(new Date().getFullYear())
-            </script> Soft by Creative Tim.
-          </p>
+        <div class="row">
+            <div class="col-8 mx-auto text-center mt-1">
+                <p class="mb-0 text-secondary">
+                    Copyright © <script>
+                        document.write(new Date().getFullYear())
+                    </script> Soft by Creative Tim.
+                </p>
+            </div>
         </div>
-      </div>
     </div>
-  </footer>
-  <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
-  <!--   Core JS Files   -->
-  <script src="../assets/js/core/popper.min.js"></script>
-  <script src="../assets/js/core/bootstrap.min.js"></script>
-  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
-  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script>
+</footer>
+<!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+<!--   Core JS Files   -->
+<script src="../assets/js/core/popper.min.js"></script>
+<script src="../assets/js/core/bootstrap.min.js"></script>
+<script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+<script>
     var win = navigator.platform.indexOf('Win') > -1;
     if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        var options = {
+            damping: '0.5'
+        }
+        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
     }
-  </script>
-  <!-- GitHub buttons -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.7"></script>
+</script>
+<!-- GitHub buttons -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+<script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.7"></script>
 </body>
 
 </html>
