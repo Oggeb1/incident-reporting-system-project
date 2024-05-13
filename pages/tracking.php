6fc3172c2747d891a6c0f9f9ab0dc47bae1475db
oggeb1 <nej@nej.se>
2024-05-13 15:08:57 +0200

    Fixed XSS-protection

--- pages/tracking.php
+++ pages/tracking.php
@@ -19,9 +19,12 @@ if (empty($pageName)) {
 $browserID = $db->execute_query("SELECT browserID FROM browser WHERE browserDescription LIKE ?", [$browserName])->fetch_all();
 $pageID = $db->execute_query("SELECT pageID FROM page WHERE page.pageDescription LIKE ?", [$pageName])->fetch_all();
 
-$userID[0][0] = htmlspecialchars($userID[0][0]);
-$pageID[0][0] = htmlspecialchars($pageID[0][0]);
 $browserID[0][0] = htmlspecialchars($browserID[0][0]);
+$pageID[0][0] = htmlspecialchars($pageID[0][0]);
+
+if ($userID[0][0] != "") {
+    $userID[0][0] = htmlspecialchars($userID[0][0]);
+}
 
 //Finally insert the log with all the IDs
 $db->execute_query("INSERT INTO log (userID, pageID, ip, browserID) VALUES ((?), (?), INET6_ATON((?)), (?))", [$userID[0][0], $pageID[0][0], htmlspecialchars($_SERVER['REMOTE_ADDR']), $browserID[0][0]]);
